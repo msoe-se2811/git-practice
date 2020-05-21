e1c720189324d6fcbc7e9928135d495cd97465d6
Ultraviolet-Ninja <MalcolmRJohns740@gmail.com>
2020-05-21 12:41:21 -0500

    Changed method name

--- source.java
+++ source.java
@@ -1,11 +1,11 @@
 public static void main(String[] ignored) {
    Scanner in = new Scanner(System.in);
-   String x = f(in);
+   String x = prompt(in);
 
    System.out.println("Your name is "+x);
 }
 
-private static String f(Scanner in){
+private static String prompt(Scanner in){
   System.out.println("Please enter your name");
   return in.nextLine();
 }
